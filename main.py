5c8c951c54b2c5c01ca6c17241dff5858ff56921
KRISH708048 <krishagarwal7080@gmail.com>
2024-06-28 09:29:30 +0530

    all done

--- main.py
+++ main.py
@@ -44,7 +44,14 @@ while True:
     currentImg[0:sh, width - sw:width] = sideImg
     indHand = -1
     hands, img = detector.findHands(img)
-    print(hands)
+    # print(hands)
+    if hands:
+        for hand in hands:
+            fingersUp = detector.fingersUp(hand)
+            if fingersUp == [1, 1, 1, 1, 1]:
+                cap.release()
+                cv2.destroyAllWindows()
+                exit()
     if hands and buttonPressed is False:
         if hands[0]['type'] == 'Left' and len(hands) == 1:
 
