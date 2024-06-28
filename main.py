ecdfa740d752a709ff7b37be31346fdc909daaad
KRISH708048 <krishagarwal7080@gmail.com>
2024-06-28 09:23:53 +0530

    zoom and shrink gesture added

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ cap.set(3, width)
 cap.set(4, height)
 
 # gesture variables
-detector = HandDetector(detectionCon=0.8, maxHands=1)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 gestureDetectThreshold = 450
 buttonPressed = False
 buttonPressedCount = 0
@@ -24,6 +24,9 @@ buttonDelay = 20
 xVal, yVal = 0, 0
 fingersUp = []
 indexFinger = 2
+sDist = None
+scale = 0
+cx1, cy1 = 500, 500
 annotations = [[]]
 annotInd = -1
 annotSet = False
@@ -39,69 +42,109 @@ while True:
     currentImg = cv2.resize(currentImg, (width, height))
     sideImg = cv2.resize(img, (sw, sh))
     currentImg[0:sh, width - sw:width] = sideImg
+    indHand = -1
     hands, img = detector.findHands(img)
-
+    print(hands)
     if hands and buttonPressed is False:
-        hand = hands[0]
-        fingersUp = detector.fingersUp(hand)
-        # print(fingersUp)
-        cx, cy = hand['center']
-        lmList = hand['lmList']
-        indexFinger = lmList[8][0], lmList[8][1]
-        xVal = indexFinger[0]
-        yVal = indexFinger[1]
-        indexFinger = int(np.interp(indexFinger[0], [width//2, width], [0, width])), int(np.interp(indexFinger[1]
-                                                                                    , [100, height-100], [0, height]))
-        if cy <= gestureDetectThreshold:    # gesture- left slide
-            if fingersUp == [1, 0, 0, 0, 0]:
-                # print("left")
-                buttonPressed = True
-                annotations = [[]]
-                annotSet = False
-                annotInd = -1
-                if slideNo > 0:
-                    slideNo -= 1
-            if fingersUp == [0, 0, 0, 0, 1]:    # gesture- right slide
-                # print("left")
-                buttonPressed = True
-                annotations = [[]]
-                annotInd = -1
-                annotSet = False
-                if slideNo < len(pathImages) - 1:
-                    slideNo += 1
+        if hands[0]['type'] == 'Left' and len(hands) == 1:
 
-        if fingersUp == [0, 1, 0, 0, 0]:
-            cv2.circle(currentImg, (indexFinger[0], indexFinger[1]), 10, (0, 0, 255), cv2.FILLED)
-            if not annotSet:
-                annotSet = True
-                annotInd += 1
-                annotations.append([])
-            annotations[annotInd].append(indexFinger)
+            handL = hands[0]
+            fingersUp = detector.fingersUp(handL)
+            # print(fingersUp)
+            cx, cy = handL['center']
+            lmList = handL['lmList']
+            indexFinger = lmList[8][0], lmList[8][1]
+            xVal = indexFinger[0]
+            yVal = indexFinger[1]
+            indexFinger = int(np.interp(indexFinger[0], [width // 2, width], [0, width])), int(np.interp(indexFinger[1]
+                                                                                                         , [100,
+                                                                                                            height - 100],
+                                                                                                         [0, height]))
+            if cy <= gestureDetectThreshold:  # gesture- left slide
+                if fingersUp == [1, 0, 0, 0, 0]:
+                    # print("left")
+                    buttonPressed = True
+                    annotations = [[]]
+                    annotSet = False
+                    annotInd = -1
+                    if slideNo > 0:
+                        slideNo -= 1
+                if fingersUp == [0, 0, 0, 0, 1]:  # gesture- right slide
+                    # print("left")
+                    buttonPressed = True
+                    annotations = [[]]
+                    annotInd = -1
+                    annotSet = False
+                    if slideNo < len(pathImages) - 1:
+                        slideNo += 1
+                if fingersUp == [0, 1, 1, 1, 1]:  # gesture- right slide
+                    # print("left")
+                    buttonPressed = True
+
+            if fingersUp == [0, 1, 0, 0, 0]:  # gesture- to draw
+                cv2.circle(currentImg, (indexFinger[0], indexFinger[1]), 10, (0, 0, 255), cv2.FILLED)
+                if not annotSet:
+                    annotSet = True
+                    annotInd += 1
+                    annotations.append([])
+                annotations[annotInd].append(indexFinger)
+            else:
+                annotSet = False
+
+            if fingersUp == [0, 1, 1, 1, 0]:  # gesture- to clean draw part
+                if annotations:
+                    annotations.pop(-1)
+                    annotInd -= 1
+                    annotSet = False
+                    buttonPressed = True
+
+            if fingersUp == [0, 1, 1, 0, 0] and xVal >= width // 2:  # gesture- pointer over slide
+                # print(xVal, yVal)
+                cv2.circle(currentImg, (indexFinger[0], indexFinger[1]), 10, (0, 0, 255), cv2.FILLED)
+                annotSet = False
+        if len(hands) == 2:
+            if hands[0]['type'] == "Left":
+                indHand = 0
+            else:
+                indHand = 1
+            handR = hands[indHand]
+            handL = hands[not indHand]
+            # print(handL, handR)
+            fingersUp1 = detector.fingersUp(handL)
+            fingersUp2 = detector.fingersUp(handR)
+
+            if fingersUp1 == [1, 1, 0, 0, 0] and fingersUp2 == [1, 1, 0, 0, 0]:  # gesture- for zooming and shrinking
+                lmList1 = handL['lmList']
+                lmList2 = handR['lmList']
+                if sDist is None:
+                    length, info, img = detector.findDistance(handL['center'], handR['center'], img,
+                                                              color=(255, 255, 0))
+                    sDist = length
+
+                length, info, img = detector.findDistance(handL['center'], handR['center'], img,
+                                                          color=(255, 255, 0))
+                scale = int((length - sDist) // 2)
+                cx1, cy1 = info[4:]
         else:
-            annotSet = False
+            sDist = None
+        try:
+            h1, w1, _ = currentImg.shape
+            newH, newW = ((h1 + scale) // 2) * 2, ((w1 + scale) // 2) * 2
+            currentImg = cv2.resize(currentImg, (newW, newH))
 
-        if fingersUp == [0, 1, 1, 1, 0]:
-            if annotations:
-                annotations.pop(-1)
-                annotInd -= 1
-                annotSet = False
-                buttonPressed = True
-
-        if fingersUp == [0, 1, 1, 0, 0] and xVal >= width // 2:   # gesture- pointer over slide
-            # print(xVal, yVal)
-            cv2.circle(currentImg, (indexFinger[0], indexFinger[1]), 10, (0, 0, 255), cv2.FILLED)
-            annotSet = False
-
+            img[cy1 - newH // 2:cy1 + newH // 2, cx1 - newW // 2:cx1 + newW // 2] = currentImg
+        except:
+            pass
     if buttonPressed:
         buttonPressedCount += 1
         if buttonPressedCount > buttonDelay:
             buttonPressed = False
             buttonPressedCount = 0
-    # print(annotations[annotInd])
+        # print(annotations[annotInd])
     for i in range(len(annotations)):
         for j in range(len(annotations[i])):
             if j != 0:
-                cv2.line(currentImg, annotations[i][j-1], annotations[i][j], (0, 0, 255), 5)
+                cv2.line(currentImg, annotations[i][j - 1], annotations[i][j], (0, 0, 255), 5)
     cv2.imshow("current image", currentImg)
     cv2.imshow("image", img)
 
